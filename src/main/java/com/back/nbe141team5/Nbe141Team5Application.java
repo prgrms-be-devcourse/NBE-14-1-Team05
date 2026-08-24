ca1649e704d39b8c7093ec946cf05e7180c7006c
GIoriousPurpose <vvipia@naver.com>
2026-08-24 11:20:00 +0900

    init

---
+++ src/main/java/com/back/nbe141team5/Nbe141Team5Application.java
@@ -0,0 +1,13 @@
+package com.back.nbe141team5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe141Team5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe141Team5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/nbe141team5/Nbe141Team5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.nbe141team5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe141Team5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

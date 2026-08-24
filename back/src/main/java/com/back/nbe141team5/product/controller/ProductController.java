d4991fc36647d266d3395fdfd94b669ede965a73
GIoriousPurpose <vvipia@naver.com>
2026-08-24 16:08:17 +0900

    fix: 전체상품조회, 특정상품조회 기능 수정

--- back/src/main/java/com/back/nbe141team5/product/controller/ProductController.java
+++ back/src/main/java/com/back/nbe141team5/product/controller/ProductController.java
@@ -4,14 +4,14 @@ import com.back.nbe141team5.product.dto.ProductResponse;
 import com.back.nbe141team5.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
 public class ProductController {

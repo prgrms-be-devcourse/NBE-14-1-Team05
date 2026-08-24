e2abc3ab3fa9603419ed1c76c47d19e34e5329f0
kongsni <binybinybiny03@gmail.com>
2026-08-24 14:38:42 +0900

    feat: BaseEntity 및 JPA Auditing 설정

--- back/src/main/java/com/back/nbe141team5/Nbe141Team5Application.java
+++ back/src/main/java/com/back/nbe141team5/Nbe141Team5Application.java
@@ -2,12 +2,13 @@ package com.back.nbe141team5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Nbe141Team5Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Nbe141Team5Application.class, args);
     }
-
 }
